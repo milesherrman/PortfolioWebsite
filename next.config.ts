2e9abf4f780fec7dc1571b5b6ce7315f70887447
milesherrman <milesherrman@me.com>
2025-05-14 11:42:24 +0200

    removing depricated keys

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,10 @@ import { type NextConfig } from 'next'
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   output: 'export',
   images: {
-    unoptimized: true, // <-- disable image optimization for static export
+    unoptimized: true,
   },
-}
+};
 
 export default nextConfig
